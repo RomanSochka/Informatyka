09d3f05b36cd97a14c2760d81219257479818b43
RomanSochka <crayzerg21@gmail.com>
2025-03-26 10:23:44 +0100

    Add files via upload

---
+++ Instrukcja warunkowa if/zadanie 2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    double waga, wzrost, bmi;
+
+   
+    cout << "Podaj wage (w kg): ";
+    cin >> waga;
+    cout << "Podaj wzrost (w metrach): ";
+    cin >> wzrost;
+
+    
+    bmi = waga / (wzrost * wzrost);
+
+   
+    
+    cout << "Twoje BMI wynosi: " << bmi << endl;
+
+    if (bmi < 18.5) {
+        cout << "Twój stan wagowy: Niedowaga" << endl;
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+        cout << "Twój stan wagowy: W normie" << endl;
+    } else if (bmi >= 25 && bmi <= 29.9) {
+        cout << "Twój stan wagowy: Nadwaga" << endl;
+    } else {
+        cout << "Twój stan wagowy: Otylosc" << endl;
+    }
+
+    return 0;
+}
